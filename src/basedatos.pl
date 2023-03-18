2928cd377e1cdca35c91528883106f763e200caf
GChacon8 <gabriel.chacon.alfaro@gmail.com>
2023-03-17 18:58:57 -0600

    Expansion de la base de datos

--- src/basedatos.pl
+++ src/basedatos.pl
@@ -18,14 +18,44 @@ causas(2, ["No esta conectada a alimentacion electrica.",
            "No tiene instalados los drivers correctos.",
            "No tiene tinta."]).
 
+causas(3, ["El mouse tiene suciedad en el sensor optico",
+           "El USB del mouse no esta conectado al dispositivo",
+           "El mouse ya no tiene bateria",
+           "El mouse no se ha conectado por bluetooth"]).
+
+causas(4, ["Su teclado no esta conectado al dispositivo",
+           "El USB de su teclado no esta conectado a su dispositivo",
+           "No ha conectado por bluetooth su teclado todavia",
+           "Su teclado esta sucio"]).
+
 preguntas(2, ["Ya reviso que tenga alimentacion electrica?",
               "Su impresora tiene conexion WiFi?",
               "Esta su impresora en la misma red que su computador?",
               "Es primera vez que utiliza su computador con esta impresora?",
               "Ya instalo los drivers de su impresora?"]).
 
-soluciones(2, ["Compruebe que la impresora este conectada a algun tomacorriente",
-               "Valide que el WiFi de la impresora este encendido y que la misma se conecte a la red",
-               "Valide que tanto la computadora como impresora este conectada a la misma red WiFi",
-               "Valide que su computadora reconoce la impresora",
-               "Proceda a instalarlos y pruebe de nuevo"]).
+preguntas(3, ["El sensor optico del mouse esta sucio?",
+              "El USB del mouse esta conectado al dispositivo?",
+              "El mouse tiene bateria con carga?",
+              "Ya conecto el mouse por bluetooth al dispositivo?"]).
+
+preguntas(4, ["Ya conecto su teclado al dispositivo deseado?",
+              "El USB de su teclado esta conectado al dispositivo?",
+              "Su teclado esta conectado por bluetooth al dispositivo?",
+              "Ha limpiado su teclado recientemente?"]).
+
+soluciones(2, ["Compruebe que la impresora este conectada a algun tomacorriente.",
+               "Valide que el WiFi de la impresora este encendido y que la misma se conecte a la red.",
+               "Valide que tanto la computadora como impresora este conectada a la misma red WiFi.",
+               "Valide que su computadora reconoce la impresora.",
+               "Proceda a instalarlos y pruebe de nuevo."]).
+
+soluciones(3, ["Limpie el sensor optico del mouse y evite usarlo en superficies sucias.",
+               "Conecte el USB del mouse al dispositivo, si es la primera vez que lo hace deje que se instalen los drivers o instalelos manualmente.",
+               "Cambiele las baterias al mouse, asegurese de que sean del tipo requerido.",
+               "Conecte su mouse por bluetooth al dispositivo deseado."]).
+
+soluciones(4, ["Conecte su teclado al dispositivo",
+               "Asegurese de identificar el USB de su teclado y conectelo al dispositivo",
+               "Conecte por bluetooth el telcado al su dispositivo",
+               "Retire las keycaps de su teclado y limpielo con cuidado"]).
